2c9559002c33a7435e6df4fbbcac6b4251bd3051
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-02-18 14:24:24 +0330

    added all files

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import Header from "./Components/header";
 import Slider from "./Components/Slider";
 import Info from "./Components/Info";
+import Cards from "./Components/Cards";
 
 function App() {
   const CRADS_INFO = [
     {
       id: 1,
+      img: "/img/card1.png",
       rate: 5.0,
       rateNumber: 6,
       country: "USA",
@@ -17,6 +19,7 @@ function App() {
     },
     {
       id: 2,
+      img: "/img/card2.jfif",
       rate: 5.0,
       rateNumber: 30,
       country: "USA",
@@ -26,6 +29,7 @@ function App() {
     },
     {
       id: 3,
+      img: "/img/card3.jfif",
       rate: 4.8,
       rateNumber: 2,
       country: "USA",
@@ -40,12 +44,25 @@ function App() {
       <Header />
       <Slider />
       <Info />
+      <div className="cardsSection">
+        {CRADS_INFO.map((card) => {
+          const { id, img, rate, rateNumber, country, about, cost, isOnline } =
+            card;
+          return (
+            <Cards
+              img={img}
+              title={about}
+              rate={rate}
+              rateCount={rateNumber}
+              location={country}
+              cost={cost}
+              isOnline={isOnline}
+            />
+          );
+        })}
+      </div>
     </>
   );
 }
-
-// function Cards2({ template }) {
-//   return <div className="cardsSection">{template}</div>;
-// }
 
 export { App, Header, Slider, Info };
