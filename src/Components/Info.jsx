2597db152cff2489a09914197937fb92e0f4af58
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-02-18 14:57:11 +0330

    classes/info

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,8 +1,8 @@
 export default function Info() {
   return (
     <div className="infoContainer">
-      <p className="infoHeader">Online Experiences</p>
-      <p className="infoText">
+      <p className="infoContainer__header">Online Experiences</p>
+      <p className="infoContainer__text">
         Join unique interactive activities led by one-of-a-kind hosts—all
         without leaving home.
       </p>
