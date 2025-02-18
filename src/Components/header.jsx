832eea48acc4e69bfbf0620b3791cc5d40f1f9b9
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-02-18 14:55:18 +0330

    classes/header

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="headerContainer">
-      <h1 className="headerText">airbnb</h1>
+      <h1 className="headerContainer__text">airbnb</h1>
     </div>
   );
 }
